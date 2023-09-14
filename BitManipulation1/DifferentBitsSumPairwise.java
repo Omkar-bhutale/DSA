b36aaa23b7ca00d6544c390e347b051ac2b3ea7a
Omkar-bhutale <123381719+Omkar-bhutale@users.noreply.github.com>
2023-09-14 20:07:31 +0530

    Update DifferentBitsSumPairwise.java
    1st optimization not working for bigger test cases

--- BitManipulation1/DifferentBitsSumPairwise.java
+++ BitManipulation1/DifferentBitsSumPairwise.java
@@ -1,20 +1,37 @@
 public class DifferentBitsSumPairwise {
     public int cntBits(int[] A) {
         //brute force   
-        int n = A.length;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int count = 0;
-            for(int k=0;k<32;k++){
-                if(((A[i]>>k)&1) != ((A[j]>>k)&1)){
-                    count++;
-                }
+    //     int n = A.length;
+    //     int ans=0;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=i+1;j<n;j++){
+    //             int count = 0;
+    //         for(int k=0;k<32;k++){
+    //             if(((A[i]>>k)&1) != ((A[j]>>k)&1)){
+    //                 count++;
+    //             }
+    //         }
+    //         ans+=(2*count);
+    //         }
+    //     }
+    //     return ans;
+    // }
+    //step 1 opti //not works for hard cases
+    public class Solution {
+    public int cntBits(int[] A) {
+        long ans=0;
+        for(int i=0;i<32;i++){
+            int ones=0;
+            int zeros=0;
+            for(int j=0;j<A.length;j++){
+               if(((A[j]>>i)&1)==1){ ones++;}
+               else zeros++;
             }
-            ans+=(2*count);
-            }
+            ans+= (2*ones*zeros);
+            
         }
         return ans;
     }
-    
+}
+
 }
