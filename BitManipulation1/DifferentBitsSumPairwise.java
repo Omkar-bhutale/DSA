941b086cb37c3f095b69691e98516513001af6f9
Omkar-bhutale <123381719+Omkar-bhutale@users.noreply.github.com>
2023-09-14 20:09:57 +0530

    Update DifferentBitsSumPairwise.java
    work for all

--- BitManipulation1/DifferentBitsSumPairwise.java
+++ BitManipulation1/DifferentBitsSumPairwise.java
@@ -20,19 +20,38 @@ public class DifferentBitsSumPairwise {
     //step 1 opti //not works for hard cases
     
    
-        long ans=0;
-        for(int i=0;i<32;i++){
-            int ones=0;
-            int zeros=0;
-            for(int j=0;j<A.length;j++){
-               if(((A[j]>>i)&1)==1){ ones++;}
-               else zeros++;
-            }
-            ans+= (2*ones*zeros);
+        // long ans=0;
+        // for(int i=0;i<32;i++){
+        //     int ones=0;
+        //     int zeros=0;
+        //     for(int j=0;j<A.length;j++){
+        //        if(((A[j]>>i)&1)==1){ ones++;}
+        //        else zeros++;
+        //     }
+        //     ans+= (2*ones*zeros);
             
+        // }
+        // return ans;
+
+        //most working approch
+        public class Solution {
+    public int cntBits(List<Integer> a) {
+        int[] count1=new int[32];
+        for (int n:a) {
+            for (int i=0; i<32; i++) {
+                count1[i]+=n & 1;
+                n=n>>1;
+            }
         }
-        return ans;
-        
+        long res=0;
+        for (int i=0; i<32; i++) {
+            res+=((long)count1[i]*(a.size()-count1[i]))%1000000007;
+        }
+        return (int)(2*res%1000000007);
+    }
+}
+
+
   
 
     }
