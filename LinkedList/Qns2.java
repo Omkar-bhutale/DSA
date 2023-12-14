84b1d7de461989636d774628af24d71efe518609
Omkar-bhutale <123381719+Omkar-bhutale@users.noreply.github.com>
2023-12-15 01:04:13 +0530

    Update Qns2.java

--- LinkedList/Qns2.java
+++ LinkedList/Qns2.java
@@ -10,13 +10,34 @@ Output : 1 -> 2 -> 3 -> 4 -> 5 -> 6
 
 
 public class Qns2 {
-	static void insertAfterNode(Node pos,Node node) {
-		
-		node.nextNode = pos.nextNode;
-		pos.nextNode = node;
-		
-		
-	}
+static void push_at(Node head ,int newElement, int position) {     
+    Node newNode = new Node(); 
+    newNode.data = newElement;
+    newNode.next = null;
+
+    if(position < 1) {
+      System.out.print("\nposition should be >= 1.");
+    } else if (position == 1) {
+      newNode.next = head;
+      head = newNode;
+    } else {
+      
+      Node temp = new Node();
+      temp = head;
+      for(int i = 1; i < position-1; i++) {
+        if(temp != null) {
+          temp = temp.next;
+        }
+      }
+   
+      if(temp != null) {
+        newNode.next = temp.next;
+        temp.next = newNode;  
+      } else {
+        System.out.print("\nThe previous node is null.");
+      }       
+    }
+  } 
 	public static void main(String[] args) {
 		Node head = new Node(1);
 		
